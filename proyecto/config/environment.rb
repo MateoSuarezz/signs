3b0e7a04a381d89a93bd534653c7811d5916ed16
Lara Combina <65500848+larchadrg@users.noreply.github.com>
2023-08-22 12:37:12 -0300

    setup of rspec

---
+++ proyecto/config/environment.rb
@@ -0,0 +1,4 @@
+# config/environment.rb
+  db_options = YAML.load(File.read('./config/database.yml'))
+  environment = ENV['APP_ENV'] || 'development'
+  ActiveRecord::Base.establish_connection(db_options[environment])

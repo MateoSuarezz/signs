caa2b1b5bd1d9f36fa8aa5bca7fc9188ab33b2f4
rocioarias <rocioarias888@gmail.com>
2023-09-03 12:03:03 -0300

    Test #3

--- proyecto/db/migrate/20230602143715_add_relatives.rb
+++ proyecto/db/migrate/20230602143715_add_relatives.rb
@@ -1,5 +1,5 @@
 class AddRelatives < ActiveRecord::Migration[7.0]
   def change
-    add_column :questions, :module_id, :integer 
+    add_column :question, :module_id, :integer 
   end
 end
--- proyecto/db/schema.rb
+++ proyecto/db/schema.rb
@@ -10,5 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_02_143715) do
+  create_table "assessments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "correct_answers"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_assessments_on_user_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "description"
+    t.string "content_link"
+  end
+
+  create_table "modules", force: :cascade do |t|
+    t.string "name"
+    t.integer "point"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "person", force: :cascade do |t|
+    t.string "name"
+    t.string "mail"
+    t.string "password"
+  end
+
+  create_table "question", force: :cascade do |t|
+    t.integer "question"
+    t.boolean "answer"
+    t.integer "module_id"
+  end
+
 end

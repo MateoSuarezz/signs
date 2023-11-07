b0680ed57bc99573f3c81b5ebd789474958c2439
Lara Combina <lara.combina11@gmail.com>
2023-11-07 10:56:13 -0300

    update of rubocop ignored exceptions

--- proyecto/models/card.rb
+++ proyecto/models/card.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# cards model
 class Card < ActiveRecord::Base
   has_one :module
   validates :content_link, presence: true, format: { with: %r{\A/images/} }
--- proyecto/models/modules.rb
+++ proyecto/models/modules.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# modules model
 class Modules < ActiveRecord::Base
   has_many :questions
 end

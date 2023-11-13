50b56b151a800ff689959424c23b1812971de21c
MateoSuarezz <mateo99suarez@gmail.com>
2023-11-13 15:19:33 -0300

    Add module greetings

--- proyecto/db/schema.rb
+++ proyecto/db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,51 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_231_010_170_859) do
-  create_table 'cards', force: :cascade do |t|
-    t.string 'description'
-    t.string 'content_link'
-    t.integer 'module_id'
+ActiveRecord::Schema[7.0].define(version: 2023_10_10_170859) do
+  create_table "cards", force: :cascade do |t|
+    t.string "description"
+    t.string "content_link"
+    t.integer "module_id"
   end
 
-  create_table 'modules', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'points'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "modules", force: :cascade do |t|
+    t.string "name"
+    t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'person', force: :cascade do |t|
-    t.string 'name'
-    t.string 'mail'
-    t.string 'password'
+  create_table "person", force: :cascade do |t|
+    t.string "name"
+    t.string "mail"
+    t.string "password"
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.boolean 'answer'
-    t.string 'question'
-    t.string 'content_link'
-    t.integer 'module_id'
+  create_table "questions", force: :cascade do |t|
+    t.boolean "answer"
+    t.string "question"
+    t.string "content_link"
+    t.integer "module_id"
   end
 
-  create_table 'responses', force: :cascade do |t|
-    t.integer 'users_id', null: false
-    t.integer 'questions_id', null: false
-    t.boolean 'correct_answer', default: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['questions_id'], name: 'index_responses_on_questions_id'
-    t.index ['users_id'], name: 'index_responses_on_users_id'
+  create_table "responses", force: :cascade do |t|
+    t.integer "users_id", null: false
+    t.integer "questions_id", null: false
+    t.boolean "correct_answer", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["questions_id"], name: "index_responses_on_questions_id"
+    t.index ["users_id"], name: "index_responses_on_users_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'password_digest'
-    t.string 'name'
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.string "name"
   end
 
-  add_foreign_key 'responses', 'questions', column: 'questions_id'
-  add_foreign_key 'responses', 'users', column: 'users_id'
+  add_foreign_key "responses", "questions", column: "questions_id"
+  add_foreign_key "responses", "users", column: "users_id"
 end
